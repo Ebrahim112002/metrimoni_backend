abf26c62110c57d686688dcc119c243fafe44db0
Ebrahim112002 <md1847094@gmail.com>
2026-01-23 20:06:45 +0600

    create biodatas

--- db.js
+++ db.js
@@ -1,35 +1,35 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const admin = require('firebase-admin');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const admin = require('firebase-admin');
 
-const setupConfig = async () => {
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      }),
-    });
-  }
+// const setupConfig = async () => {
+//   if (!admin.apps.length) {
+//     admin.initializeApp({
+//       credential: admin.credential.cert({
+//         projectId: process.env.FIREBASE_PROJECT_ID,
+//         privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//       }),
+//     });
+//   }
 
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dit9xra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-  const client = new MongoClient(uri, {
-    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
-  });
+//   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dit9xra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//   const client = new MongoClient(uri, {
+//     serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true },
+//   });
 
-  await client.connect();
-  const db = client.db('matrimonial');
-  console.log('✅ MongoDB and Firebase Admin Connected');
+//   await client.connect();
+//   const db = client.db('matrimonial');
+//   console.log('✅ MongoDB and Firebase Admin Connected');
   
-  return {
-    users: db.collection('users'),
-    members: db.collection('members'),
-    payments: db.collection('payments'),
-    premiumRequests: db.collection('premiumRequests'),
-    success_counter: db.collection('success_counter'),
-    favourites: db.collection('favourites'),
-    contactRequests: db.collection('contactRequests'),
-  };
-};
+//   return {
+//     users: db.collection('users'),
+//     members: db.collection('members'),
+//     payments: db.collection('payments'),
+//     premiumRequests: db.collection('premiumRequests'),
+//     success_counter: db.collection('success_counter'),
+//     favourites: db.collection('favourites'),
+//     contactRequests: db.collection('contactRequests'),
+//   };
+// };
 
-module.exports = setupConfig;
+// module.exports = setupConfig;
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const setupConfig = require('./db');
-const createRoutes = require('./routes');
+// const express = require('express');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
+// const setupConfig = require('./db');
+// const createRoutes = require('./routes');
 
-dotenv.config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const app = express();
-const port = process.env.PORT || 3000;
+// dotenv.config();
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: ['http://localhost:5173', 'https://metrimoniyal.netlify.app'], credentials: true }));
-app.use(express.json());
+// app.use(cors({ origin: ['http://localhost:5173', 'https://metrimoniyal.netlify.app'], credentials: true }));
+// app.use(express.json());
 
-async function run() {
-  try {
-    const collections = await setupConfig();
+// async function run() {
+//   try {
+//     const collections = await setupConfig();
     
-    app.use('/', createRoutes(collections, stripe));
+//     app.use('/', createRoutes(collections, stripe));
 
-    app.get('/', (req, res) => res.send('Love Matrimony server is running...'));
-    app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
-  } catch (error) {
-    console.error('Start error:', error);
-  }
-}
+//     app.get('/', (req, res) => res.send('Love Matrimony server is running...'));
+//     app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+//   } catch (error) {
+//     console.error('Start error:', error);
+//   }
+// }
 
-run();
+// run();
